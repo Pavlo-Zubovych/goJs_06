cbfda667e90e0cc9c4046019c602383611a4f423
Pavlo-Zubovych <zu10@ukr.net>
2021-11-08 23:20:37 +0200

    add 6_4

---
+++ js/task-6_4.js
@@ -0,0 +1,15 @@
+// Завдання 4
+// Отримати масив тільки неактивних користувачів (поле isActive).
+
+import users from './users.js';
+
+console.table(users);
+
+const getInactiveUsers = (users) => {
+  // твій код
+  return users.filter(function (user) {
+    return !user.isActive;
+  });
+};
+
+console.table(getInactiveUsers(users)); // [об'єкт Moore Hensley, об'єкт Ross Vazquez, об'єкт Blackburn Dotson]
