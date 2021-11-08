f8e60fd53465620a68ecea8707ddc5ab061b500f
Pavlo-Zubovych <zu10@ukr.net>
2021-11-08 23:19:40 +0200

    repair with the help

--- js/task-6_2.js
+++ js/task-6_2.js
@@ -3,7 +3,9 @@
 
 import users from './users.js';
 
-//_______так получилось___________________
+console.table(users);
+
+//_______самоділяльність___________________
 
 const testFilter = users.filter(function (user) {
   return user.eyeColor === 'blue';
@@ -11,21 +13,12 @@ const testFilter = users.filter(function (user) {
 
 console.table(testFilter);
 
-// __тут щось роблю  не правельно_________________
+// _____виконане завдання_________________
 
 const getUsersWithEyeColor = (users, color) => {
-  // твій код
-  users.filter(function (user) {
+  return users.filter(function (user) {
     return user.eyeColor === color;
   });
 };
 
-console.log(getUsersWithEyeColor(users, 'blue')); // [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
-
-// Стрелки
-// const add = (a, b, c) => {
-//   console.log(a, b, c);
-//   return a + b + c;
-// };
-// add(1, 2, 3);
-// console.log(add(1, 2, 3));
+console.table(getUsersWithEyeColor(users, 'blue')); // [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
