0c3ca3e26df11271ad08e101f6424a7b3977723e
Pavlo-Zubovych <zu10@ukr.net>
2021-11-09 19:11:37 +0200

    created

---
+++ js/task-6_5.js
@@ -0,0 +1,16 @@
+// Завдання 5
+// Отримати користувача (не масив) по email (поле email, він унікальний).
+
+import users from './users.js';
+
+console.table(users);
+
+const getUserWithEmail = (users, email) => {
+  return users.find(function (user) {
+    return user.email === email;
+  });
+  // твій код
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {об'єкт користувача Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {об'єкт користувача Elma Head}
