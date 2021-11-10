b6b67c5574bed5af9dba64c338727d149bb58942
Pavlo-Zubovych <zu10@ukr.net>
2021-11-10 21:10:00 +0200

    done

---
+++ js/task-6_8.js
@@ -0,0 +1,21 @@
+// Завдання 8
+// Масив імен всіх користувачів у яких є друг із зазначеним ім'ям.
+
+import users from './users.js';
+
+console.table(users);
+
+const getUsersWithFriend = (users, friendName) => {
+  return users.reduce(function (acc, user) {
+    if (user.friends.includes(friendName)) {
+      acc.push(user.name);
+    }
+    return acc;
+  }, []);
+  // твій код
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker'));
+// [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+// [ 'Elma Head', 'Sheree Anthony' ]
