3bb78a7d4b533449a09c95d1ba663fd36cae2d97
Pavlo-Zubovych <zu10@ukr.net>
2021-11-13 09:24:06 +0200

    add =>

--- js/task-6_2.js
+++ js/task-6_2.js
@@ -7,18 +7,21 @@ console.table(users);
 
 //_______самоділяльність___________________
 
-const testFilter = users.filter(function (user) {
-  return user.eyeColor === 'blue';
-});
+// const testFilter = users.filter(function (user) {
+//   return user.eyeColor === 'blue';
+// });
 
-console.table(testFilter);
+// console.table(testFilter);
 
 // _____виконане завдання_________________
 
-const getUsersWithEyeColor = (users, color) => {
-  return users.filter(function (user) {
-    return user.eyeColor === color;
-  });
-};
+// const getUsersWithEyeColor = (users, color) => {
+//   return users.filter(function (user) {
+//     return user.eyeColor === color;
+//   });
+// };
+
+const getUsersWithEyeColor = (users, color) =>
+  users.filter((user) => user.eyeColor === color);
 
 console.table(getUsersWithEyeColor(users, 'blue')); // [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
