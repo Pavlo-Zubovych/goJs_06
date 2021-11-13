6814c95d27908bf1fc3e6016753b7c4c7f8952d2
Pavlo-Zubovych <zu10@ukr.net>
2021-11-13 09:18:23 +0200

    add =>

--- js/task-6_1.js
+++ js/task-6_1.js
@@ -5,14 +5,22 @@ import users from './users.js';
 
 console.log(users);
 
+// const getUserNames = (users) => {
+//   // твій код
+//   const userName = [];
+//   users.forEach(function (user) {
+//     userName.push(user.name);
+//   });
+//   return userName;
+// };
+
 const getUserNames = (users) => {
   // твій код
   const userName = [];
-  users.forEach(function (user) {
-    userName.push(user.name);
-  });
+  users.forEach((user) => userName.push(user.name));
   return userName;
 };
 
 console.log(getUserNames(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', '
+//Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
